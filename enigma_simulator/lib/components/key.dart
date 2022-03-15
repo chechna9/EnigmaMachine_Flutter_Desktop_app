f86475c68cea16cf61dec75167ed45c114f60f09
chechna9 <ja_bouchouareb@esi.dz>
2022-03-15 13:04:08 +0100

    init repo

---
+++ enigma_simulator/lib/components/key.dart
@@ -0,0 +1,15 @@
+import 'package:fluent_ui/fluent_ui.dart';
+
+class KeyWindow extends StatefulWidget {
+  const KeyWindow({Key? key}) : super(key: key);
+
+  @override
+  State<KeyWindow> createState() => _KeyWindowState();
+}
+
+class _KeyWindowState extends State<KeyWindow> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
