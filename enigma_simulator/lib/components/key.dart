12f37c841254535069ce2b7c1aa55c097f004d17
chechna9 <ja_bouchouareb@esi.dz>
2022-03-18 21:42:28 +0100

    added logic to the key

--- enigma_simulator/lib/components/key.dart
+++ enigma_simulator/lib/components/key.dart
@@ -10,51 +10,6 @@ class KeyWindow extends StatefulWidget {
 }
 
 class _KeyWindowState extends State<KeyWindow> {
-  List<String> rotors = ["R1", "R2", "R3"];
-  List<String> decalageValues = [
-    "0",
-    "1",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "10",
-    "11",
-    "12",
-    "13",
-    "14",
-    "15",
-    "16",
-    "17",
-    "18",
-    "19",
-    "20",
-    "21",
-    "22",
-    "23",
-    "24",
-    "25"
-  ];
-
-  //first rotor conrollers
-  String firstRotor = "R1";
-  String firstRotorDirection = "D";
-  String firstRotorSigne = "+";
-  String firstRotorDecalage = "1";
-  //Second rotor values
-  String secondRotor = "R2";
-  String secondRotorDirection = "D";
-  String secondRotorSigne = "+";
-  String secondRotorDecalage = "1";
-  //third rotor values
-  String thirdRotor = "R3";
-  String thirdRotorDirection = "D";
-  String thirdRotorSigne = "+";
-  String thirdRotorDecalage = "1";
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -67,208 +22,349 @@ class _KeyWindowState extends State<KeyWindow> {
             fontSize: 24,
           ),
         ),
+        const SizedBox(
+          height: 20,
+        ),
         Column(
           // crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            //first key
+            //first keyField
+            KeyField(
+              info: RotorInfo(
+                rotorNumber: firstRotor,
+                rotorDirection: firstRotorDirection,
+                rotorSign: firstRotorSigne,
+                rotorDecalage: firstRotorDecalage,
+              ),
+              rotorNumber: (e) {
+                setState(() {
+                  firstRotor = e;
+                });
+              },
+              rotorDirection: (e) {
+                setState(() {
+                  firstRotorDirection = e;
+                });
+              },
+              rotorSign: (e) {
+                setState(() {
+                  firstRotorSigne = e;
+                });
+              },
+              rotorDecalage: (e) {
+                setState(() {
+                  firstRotorDecalage = e;
+                });
+              },
+            ),
             const SizedBox(
               height: 10,
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 8,
-                    vertical: 5,
-                  ),
-                  decoration: BoxDecoration(
-                    color: myBlue,
-                    borderRadius: BorderRadius.circular(15),
-                  ),
-                  child: DropdownButton(
-                    isDense: true,
-                    value: firstRotor,
-                    dropdownColor: myBlue,
-                    borderRadius: BorderRadius.circular(15),
-                    icon: const Icon(
-                      Icons.keyboard_arrow_down,
-                      color: myGreen,
-                    ),
-                    style: const TextStyle(
-                      fontFamily: "Poppins",
-                      color: Colors.white,
-                      fontWeight: FontWeight.w500,
-                      fontSize: 18,
-                    ),
-                    underline: Container(),
-                    onChanged: ((value) => {}),
-                    items: rotors
-                        .map(
-                          (e) => DropdownMenuItem(
-                            value: e,
-                            child: Text(e),
-                          ),
-                        )
-                        .toList(),
-                  ),
-                ),
-                const SizedBox(
-                  width: 15,
-                ),
-                Container(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 8,
-                    vertical: 5,
-                  ),
-                  decoration: BoxDecoration(
-                    color: myBlue,
-                    borderRadius: BorderRadius.circular(15),
-                  ),
-                  child: DropdownButton(
-                    isDense: true,
-                    value: firstRotorDirection,
-                    dropdownColor: myBlue,
-                    borderRadius: BorderRadius.circular(15),
-                    icon: const Icon(
-                      Icons.keyboard_arrow_down,
-                      color: myGreen,
-                    ),
-                    style: const TextStyle(
-                      fontFamily: "Poppins",
-                      color: Colors.white,
-                      fontWeight: FontWeight.w500,
-                      fontSize: 18,
-                    ),
-                    underline: Container(),
-                    onChanged: ((value) => {}),
-                    items: ["D", "G"]
-                        .map(
-                          (e) => DropdownMenuItem(
-                            value: e,
-                            child: Text(e),
-                          ),
-                        )
-                        .toList(),
-                  ),
-                ),
-                const SizedBox(
-                  width: 15,
-                ),
-                Container(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 8,
-                    vertical: 5,
-                  ),
-                  decoration: BoxDecoration(
-                    color: myBlue,
-                    borderRadius: BorderRadius.circular(15),
-                  ),
-                  child: DropdownButton(
-                    isDense: true,
-                    value: firstRotorSigne,
-                    dropdownColor: myBlue,
-                    borderRadius: BorderRadius.circular(15),
-                    icon: const Icon(
-                      Icons.keyboard_arrow_down,
-                      color: myGreen,
-                    ),
-                    style: const TextStyle(
-                      fontFamily: "Poppins",
-                      color: Colors.white,
-                      fontWeight: FontWeight.w500,
-                      fontSize: 18,
-                    ),
-                    menuMaxHeight: 200,
-                    underline: Container(),
-                    onChanged: ((value) => {}),
-                    items: ["+", "-"]
-                        .map(
-                          (e) => DropdownMenuItem(
-                            value: e,
-                            child: Text(e.toString()),
-                          ),
-                        )
-                        .toList(),
-                  ),
-                ),
-                const SizedBox(
-                  width: 15,
-                ),
-                Container(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 8,
-                    vertical: 5,
-                  ),
-                  decoration: BoxDecoration(
-                    color: myBlue,
-                    borderRadius: BorderRadius.circular(15),
-                  ),
-                  child: DropdownButton(
-                    isDense: true,
-                    value: firstRotorDecalage,
-                    dropdownColor: myBlue,
-                    borderRadius: BorderRadius.circular(15),
-                    icon: const Icon(
-                      Icons.keyboard_arrow_down,
-                      color: myGreen,
-                    ),
-                    style: const TextStyle(
-                      fontFamily: "Poppins",
-                      color: Colors.white,
-                      fontWeight: FontWeight.w500,
-                      fontSize: 18,
-                    ),
-                    menuMaxHeight: 200,
-                    underline: Container(),
-                    onChanged: ((value) => {}),
-                    items: decalageValues
-                        .map(
-                          (e) => DropdownMenuItem(
-                            value: e,
-                            child: Text(e.toString()),
-                          ),
-                        )
-                        .toList(),
-                  ),
-                ),
-              ],
+            KeyField(
+              info: RotorInfo(
+                rotorNumber: secondRotor,
+                rotorDirection: secondRotorDirection,
+                rotorSign: secondRotorSigne,
+                rotorDecalage: secondRotorDecalage,
+              ),
+              rotorNumber: (e) {
+                setState(() {
+                  secondRotor = e;
+                });
+              },
+              rotorDirection: (e) {
+                setState(() {
+                  secondRotorDirection = e;
+                });
+              },
+              rotorSign: (e) {
+                setState(() {
+                  secondRotorSigne = e;
+                });
+              },
+              rotorDecalage: (e) {
+                setState(() {
+                  secondRotorDecalage = e;
+                });
+              },
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            KeyField(
+              info: RotorInfo(
+                rotorNumber: thirdRotor,
+                rotorDirection: thirdRotorDirection,
+                rotorSign: thirdRotorSigne,
+                rotorDecalage: thirdRotorDecalage,
+              ),
+              rotorNumber: (e) {
+                setState(() {
+                  thirdRotor = e;
+                });
+              },
+              rotorDirection: (e) {
+                setState(() {
+                  thirdRotorDirection = e;
+                });
+              },
+              rotorSign: (e) {
+                setState(() {
+                  thirdRotorSigne = e;
+                });
+              },
+              rotorDecalage: (e) {
+                setState(() {
+                  thirdRotorDecalage = e;
+                });
+              },
             ),
           ],
         ),
         const SizedBox(
-          height: 15,
+          height: 20,
         ),
+        RotorConfigButton(),
+      ],
+    );
+  }
+}
+
+class RotorInfo {
+  String rotorNumber;
+  String rotorDirection;
+  String rotorSign;
+  String rotorDecalage;
+  RotorInfo(
+      {required this.rotorDecalage,
+      required this.rotorDirection,
+      required this.rotorNumber,
+      required this.rotorSign});
+}
+
+class RotorConfigButton extends StatelessWidget {
+  const RotorConfigButton({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    bool _error = false;
+
+    return Container(
+      decoration: BoxDecoration(
+        color: myBlue,
+        borderRadius: BorderRadius.circular(10),
+        // ignore: prefer_const_literals_to_create_immutables
+        boxShadow: [
+          BoxShadow(
+            color: _error ? Colors.red : myGreen,
+            offset: const Offset(4, 4),
+          ),
+        ],
+      ),
+      child: TextButton(
+        onPressed: _error ? null : () => {},
+        style: TextButton.styleFrom(
+          onSurface: myBlue,
+          padding: const EdgeInsets.symmetric(
+            vertical: 16,
+            horizontal: 8,
+          ),
+        ),
+        child: const Text(
+          "Configurer Rotors",
+          style: TextStyle(
+            fontFamily: "Poppins",
+            color: Colors.white,
+            fontWeight: FontWeight.w500,
+            fontSize: 18,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class KeyField extends StatefulWidget {
+  final RotorInfo info;
+  final Function rotorNumber;
+  final Function rotorDirection;
+  final Function rotorSign;
+  final Function rotorDecalage;
+
+  const KeyField({
+    Key? key,
+    required this.info,
+    required this.rotorNumber,
+    required this.rotorDirection,
+    required this.rotorSign,
+    required this.rotorDecalage,
+  }) : super(key: key);
+
+  @override
+  State<KeyField> createState() => _KeyFieldState();
+}
+
+class _KeyFieldState extends State<KeyField> {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
         Container(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 8,
+            vertical: 5,
+          ),
           decoration: BoxDecoration(
             color: myBlue,
-            borderRadius: BorderRadius.circular(10),
-            // ignore: prefer_const_literals_to_create_immutables
-            boxShadow: [
-              const BoxShadow(
-                color: myGreen,
-                offset: Offset(4, 4),
-              ),
-            ],
+            borderRadius: BorderRadius.circular(15),
           ),
-          child: TextButton(
-            onPressed: () => {},
-            style: TextButton.styleFrom(
-              onSurface: myBlue,
-              padding: const EdgeInsets.symmetric(
-                vertical: 16,
-                horizontal: 8,
-              ),
+          child: DropdownButton(
+            isDense: true,
+            value: widget.info.rotorNumber,
+            dropdownColor: myBlue,
+            borderRadius: BorderRadius.circular(15),
+            icon: const Icon(
+              Icons.keyboard_arrow_down,
+              color: myGreen,
             ),
-            child: const Text(
-              "Configurer Rotors",
-              style: TextStyle(
-                fontFamily: "Poppins",
-                color: Colors.white,
-                fontWeight: FontWeight.w500,
-                fontSize: 18,
-              ),
+            style: const TextStyle(
+              fontFamily: "Poppins",
+              color: Colors.white,
+              fontWeight: FontWeight.w500,
+              fontSize: 18,
             ),
+            underline: Container(),
+            onChanged: ((value) => widget.rotorNumber(value)),
+            items: rotors
+                .map(
+                  (e) => DropdownMenuItem(
+                    value: e,
+                    child: Text(e),
+                  ),
+                )
+                .toList(),
+          ),
+        ),
+        const SizedBox(
+          width: 15,
+        ),
+        Container(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 8,
+            vertical: 5,
+          ),
+          decoration: BoxDecoration(
+            color: myBlue,
+            borderRadius: BorderRadius.circular(15),
+          ),
+          child: DropdownButton(
+            isDense: true,
+            value: widget.info.rotorDirection,
+            dropdownColor: myBlue,
+            borderRadius: BorderRadius.circular(15),
+            icon: const Icon(
+              Icons.keyboard_arrow_down,
+              color: myGreen,
+            ),
+            style: const TextStyle(
+              fontFamily: "Poppins",
+              color: Colors.white,
+              fontWeight: FontWeight.w500,
+              fontSize: 18,
+            ),
+            underline: Container(),
+            onChanged: ((value) => widget.rotorDirection(value)),
+            items: ["D", "G"]
+                .map(
+                  (e) => DropdownMenuItem(
+                    value: e,
+                    child: Text(e),
+                  ),
+                )
+                .toList(),
+          ),
+        ),
+        const SizedBox(
+          width: 15,
+        ),
+        Container(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 8,
+            vertical: 5,
+          ),
+          decoration: BoxDecoration(
+            color: myBlue,
+            borderRadius: BorderRadius.circular(15),
+          ),
+          child: DropdownButton(
+            isDense: true,
+            value: widget.info.rotorSign,
+            dropdownColor: myBlue,
+            borderRadius: BorderRadius.circular(15),
+            icon: const Icon(
+              Icons.keyboard_arrow_down,
+              color: myGreen,
+            ),
+            style: const TextStyle(
+              fontFamily: "Poppins",
+              color: Colors.white,
+              fontWeight: FontWeight.w500,
+              fontSize: 18,
+            ),
+            menuMaxHeight: 200,
+            underline: Container(),
+            onChanged: ((value) => widget.rotorSign(value)),
+            items: ["+", "-"]
+                .map(
+                  (e) => DropdownMenuItem(
+                    value: e,
+                    child: Text(e.toString()),
+                  ),
+                )
+                .toList(),
+          ),
+        ),
+        const SizedBox(
+          width: 15,
+        ),
+        Container(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 8,
+            vertical: 5,
+          ),
+          decoration: BoxDecoration(
+            color: myBlue,
+            borderRadius: BorderRadius.circular(15),
+          ),
+          child: DropdownButton(
+            isDense: true,
+            value: widget.info.rotorDecalage,
+            dropdownColor: myBlue,
+            borderRadius: BorderRadius.circular(15),
+            icon: const Icon(
+              Icons.keyboard_arrow_down,
+              color: myGreen,
+            ),
+            style: const TextStyle(
+              fontFamily: "Poppins",
+              color: Colors.white,
+              fontWeight: FontWeight.w500,
+              fontSize: 18,
+            ),
+            menuMaxHeight: 200,
+            underline: Container(),
+            onChanged: ((value) => widget.rotorDecalage(value)),
+            items: decalageValues
+                .map(
+                  (e) => DropdownMenuItem(
+                    value: e,
+                    child: Text(e.toString()),
+                  ),
+                )
+                .toList(),
           ),
         ),
       ],
--- enigma_simulator/lib/constants.dart
+++ enigma_simulator/lib/constants.dart
@@ -3,3 +3,48 @@ import 'package:flutter/material.dart';
 const Color myGrey = Color(0xff506266);
 const Color myBlue = Color(0xff10454f);
 const Color myGreen = Color(0xffbde038);
+//first rotor conrollers
+String firstRotor = "R1";
+String firstRotorDirection = "D";
+String firstRotorSigne = "+";
+String firstRotorDecalage = "1";
+//Second rotor values
+String secondRotor = "R2";
+String secondRotorDirection = "D";
+String secondRotorSigne = "+";
+String secondRotorDecalage = "1";
+//third rotor values
+String thirdRotor = "R3";
+String thirdRotorDirection = "D";
+String thirdRotorSigne = "+";
+String thirdRotorDecalage = "1";
+//for rotors
+List<String> rotors = ["R1", "R2", "R3"];
+List<String> decalageValues = [
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "11",
+  "12",
+  "13",
+  "14",
+  "15",
+  "16",
+  "17",
+  "18",
+  "19",
+  "20",
+  "21",
+  "22",
+  "23",
+  "24",
+  "25"
+];
